4eea1263c9763d25f849dcb1321e3086a716a67d
sedar <desiradjysedar@gmail.com>
2023-04-17 23:19:12 +0200

    first commit

---
+++ src/components/Cart.js
@@ -0,0 +1,23 @@
+function Cart(){
+    const monsterePrix = 8;
+    const lierrePrix = 10;
+    const FleursPrix = 15;
+
+    const prixTotal = monsterePrix + lierrePrix + 
+    FleursPrix;
+
+    return (
+        <div>
+            <h2>Panier</h2>
+            <ul>
+                <li>Monstera: {monsterePrix}€</li>
+                <li>Liere: {lierrePrix}€</li>
+                <li>Fleurs: {FleursPrix}€</li>
+            </ul>
+            Total : {prixTotal}€
+        </div>
+    )
+
+}
+
+export default Cart
